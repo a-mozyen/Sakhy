9369bd6d79a177c19cf86afa8510185cf119f450
AMozy <a.mozyen@gmail.com>
2023-12-03 14:59:54 +0300

    .

--- Sakhy/.venv/Scripts/activate.bat
+++ Sakhy/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\github\shared-projects\Sakhy\.venv
+set VIRTUAL_ENV=D:\github\Sakhy\Sakhy\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
