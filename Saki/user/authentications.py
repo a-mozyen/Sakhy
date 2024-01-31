0f0dd26b923467c09e3415639c59388033ec2092
AMozy <a.mozyen@gmail.com>
2024-01-31 18:55:52 +0300

    modify custom auth

--- Saki/user/authentications.py
+++ Saki/user/authentications.py
@@ -2,25 +2,32 @@ from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from .models import User
 from datetime import timedelta, datetime
-from keys import jwt_secret
+from keys import access_token_key, refresh_token_key
 import jwt
+from Saki.variables import access_token_expiry, refresh_token_expiry
 
 
 class CustomAuthentication(BaseAuthentication):
     def authenticate(self, request):
         try:
-            token = request.COOKIES.get("jwt")
-
-            if not token:
-                return None
-
-            payload = jwt.decode(token, key=jwt_secret, algorithms=["HS256"])
-            user = User.objects.get(id=payload["id"])
-
+            token = request.headers.get("access_token")
+            return token
         except:
-            raise AuthenticationFailed("Unauthorized")
+            return Exception("Unauthorized")
+        
+        # try:
+        #     token = request.COOKIES.get("jwt")
 
-        return (user, None)
+        #     if not token:
+        #         return None
+
+        #     payload = jwt.decode(token, key=access_token_key, algorithms=["HS256"])
+        #     user = User.objects.get(id=payload["id"])
+
+        # except:
+        #     raise AuthenticationFailed("Unauthorized")
+
+        # return (user, None)
 
 
 def create_token(id: int, username: str, phone: str):
@@ -28,8 +35,19 @@ def create_token(id: int, username: str, phone: str):
         id=id,
         username=username,
         phone=phone,
-        exp=datetime.utcnow() + timedelta(hours=2),
+        exp=access_token_expiry,
         iat=datetime.utcnow(),
     )
-    token = jwt.encode(payload=payload, key=jwt_secret, algorithm="HS256")
+    token = jwt.encode(payload=payload, key=access_token_key, algorithm="HS256")
     return token
+
+def create_refresh_token(id: int, username: str, phone: str):
+    payload = dict(
+        id=id,
+        username=username,
+        phone=phone,
+        exp=refresh_token_expiry,
+        iat=datetime.utcnow(),
+    )
+    refresh_token = jwt.encode(payload=payload, key=refresh_token_key, algorithm="HS256")
+    return refresh_token
